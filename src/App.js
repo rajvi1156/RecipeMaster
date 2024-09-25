2b3ef9738d481f7c12bc21c3452bee7e4fedf111
rajvi1156 <rajvisoneji1156@gmail.com>
2024-09-25 15:23:08 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-        Hello this is  demo text to see the live site is working properly or not you can remove this whenever you need
+        demo text
     </h1>
     </div>
   );
