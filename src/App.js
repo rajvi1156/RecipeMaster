67287bc7e4bd39692a51632d021913dfefd38cb0
rajvi1156 <rajvisoneji1156@gmail.com>
2024-09-25 15:21:13 +0530

    title change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-        Hello this is the demo text to see the live site is working properly or not you can remove this whenever you need
+        Hello this is  demo text to see the live site is working properly or not you can remove this whenever you need
     </h1>
     </div>
   );
